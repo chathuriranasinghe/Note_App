1744843c8b5ef27e7d14c8b054fdcb1e47607185
Chathuri Ranasinghe <120873491+chathuriranasinghe@users.noreply.github.com>
2025-01-04 12:49:29 +0530

    Modify the app

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Languages\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Languages\\Flutter\\note_app_CEF5135\\note_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Languages\\Flutter\\flutter"
+  "PROJECT_DIR=D:\\Languages\\Flutter\\note_app_CEF5135\\note_app"
+  "FLUTTER_ROOT=D:\\Languages\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Languages\\Flutter\\note_app_CEF5135\\note_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Languages\\Flutter\\note_app_CEF5135\\note_app"
+  "FLUTTER_TARGET=D:\\Languages\\Flutter\\note_app_CEF5135\\note_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Languages\\Flutter\\note_app_CEF5135\\note_app\\.dart_tool\\package_config.json"
+)
